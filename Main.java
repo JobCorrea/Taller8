eb9488683b2271a39be58fb758883787995b3f4e
Job Daniel Correa <jobcorre0912@gmail.com>
2026-05-12 22:13:17 -0500

    Iniziaciones de prueba de objetos Vehiculo y coche

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main{
     public static void main(String[] args) {
-        
+        Vehiculo vh = new Vehiculo("BMW", 80);
+        vh.mostrarVehiculo();
+
+        Coche ch = new Coche("Mazda", 100, 4);
+        ch.mostrarVehiculo();
+        System.out.println();
     }
 }
